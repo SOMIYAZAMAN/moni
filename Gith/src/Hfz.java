f72083271d5f61ee9b440a2bea961e9e8c8237c8
user <user@172.16.108.70>
2015-11-22 22:17:18 +0500

    2ndchange

--- Gith/src/Hfz.java
+++ Gith/src/Hfz.java
@@ -4,6 +4,8 @@ public class Hfz {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Lets play with github");
+		System.out.print("2nd step");
+		System.out.print("program in execution");
 	}
 
 }
